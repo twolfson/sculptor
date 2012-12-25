9e5e5f27185b4794bb6c58f380b96eda9ddbdf9d
Todd Wolfson <todd@twolfson.com>
2012-12-25 15:36:45 -0800

    Things are passing. woot. Need to finish up tests though

--- test/sculptor_test.js
+++ test/sculptor_test.js
@@ -1,3 +1,17 @@
-var sculptor = require('../lib/sculptor.js');
+var Sculptor = require('../lib/sculptor.js'),
+    vowsSculptor = new Sculptor('vows');
 
-console.log('hey');
+vowsSculptor.addCommands({
+  'A Vows sculptor': function () {
+    return new Sculptor('vows');
+  },
+  'processing a set of commands': function () {
+    return '';
+  },
+  'outputs an expected suite': function () {
+  }
+});
+
+vowsSculptor.addBatch(require('./sculptor.tests.json'));
+
+vowsSculptor['export'](module);
