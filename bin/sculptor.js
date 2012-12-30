9521edf3d7b97b01c8667afb99fb47f082a55e5d
Todd Wolfson <todd@twolfson.com>
2012-12-29 22:31:31 -0800

    Restructuring test and module to be bin driven

---
+++ bin/sculptor.js
@@ -0,0 +1,6 @@
+// TODO: Load in lib
+// TODO: Use optimist to determine which engine to use (default: vows)
+// TODO: Use optimist to specify test dir/files
+// TODO: Use glob to expand out file paths
+// TODO: Load in tests/skeletons and commands
+// TODO: Export/run the tests -- this might be different depending on engines
--- test/sculptor_test.js
+++ test/sculptor_test.js
@@ -1,11 +1,7 @@
-var Sculptor = require('../lib/sculptor.js'),
-    vowsSculptor = new Sculptor('vows');
-
-vowsSculptor.addCommands({
+module.exports = {
   'A Vows sculptor': function (topic) {
     return {'sculptor': new Sculptor('vows')};
   },
-  // TODO: We could have that this test running is a validation of sculptor itself (how meta)
   'processing a set of commands': function (topic) {
     topic.sculptor.addCommands({
 
@@ -14,7 +10,7 @@ vowsSculptor.addCommands({
   },
   'outputs an expected suite': function () {
   }
-});
+};
 
 vowsSculptor.addBatch(require('./sculptor.tests.json'));
 
