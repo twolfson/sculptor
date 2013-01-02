53de31ac62d9e655e5a9d520e4abf3037b44438f
Todd Wolfson <todd@twolfson.com>
2013-01-01 22:18:39 -0800

    Simplified example test as done before

--- testling.upload.js
+++ testling.upload.js
@@ -2,10 +2,10 @@ var test = require('testling');
 test('the test name', function (t) {
     t.equal(2 + 2, 4);
 
-    t.log(window.navigator.appName);
+    // t.log(window.navigator.appName);
 
-    t.createWindow('http://substack.net', { t : t }, function (win, $) {
-        t.equal(win.document.title, 'The Universe of Discord');
+    // t.createWindow('http://substack.net', { t : t }, function (win, $) {
+    //     t.equal(win.document.title, 'The Universe of Discord');
         t.end();
-    });
+    // });
 });
