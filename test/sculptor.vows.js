b00467d28a994e4a6f0c9338c61a6bf18a6380f2
Todd Wolfson <todd@twolfson.com>
2013-01-06 00:59:51 -0800

    Back down to a single test suite

---
+++ test/sculptor.vows.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+module.exports = {
+  // First batch
+  'A skeletal test': function () {
+    // Get it? ;D
+    return this;
+  },
+  'can be processed via Sculptor': function () {
+    return 1;
+  },
+  'to assert items': function (topic) {
+    assert.equal(topic, 1, 'One is equal to one');
+  }
+};
